55a1040caec3b20c26db2295d44173dafc6c42d5
SarahNelviandri <sarah.210170236@mhs.unimal.ac.id>
2023-09-16 23:25:06 +0700

    array tugas9

---
+++ tugas9/assets/js/array.js
@@ -0,0 +1,53 @@
+let employess = [
+  {
+      nama: "125 Point Valorant",
+      harga: "Rp.15.000",
+      imageSrc: "assets/images/pv.png",
+      user: "Ruby Store",
+  },
+  {
+      name: "420 Point Valorant",
+      price: "Rp.49.000",
+      imageSrc: "assets/images/pv.png",
+      seller: "Ruby Store",
+  },
+  {
+      name: "700 Point Valorant",
+      price: "Rp.78.000",
+      imageSrc: "assets/images/pv.png",
+      seller: "Ruby Store",
+  },
+  {
+    name: "1375 Point Valorant",
+    price: "Rp.145.000",
+    imageSrc: "assets/images/pv.png",
+    seller: "Ruby Store",
+  },
+  {
+    name: "2400 Point Valorant",
+    price: "Rp.243.000",
+    imageSrc: "assets/images/pv.png",
+    seller: "Ruby Store",
+  },
+  {
+    name: "4000 Point Valorant",
+    price: "Rp.389.000",
+    imageSrc: "assets/images/pv.png",
+    seller: "Ruby Store",
+  },
+  {
+    name: "7000 Point Valorant",
+    price: "Rp.560.000",
+    imageSrc: "assets/images/pv.png",
+    seller: "Ruby Store",
+  },
+  {
+    name: "8150 Point Valorant",
+    price: "Rp.777.000",
+    imageSrc: "assets/images/pv.png",
+    seller: "Ruby Store",
+  },
+  
+];
+
+console.log (employess);
